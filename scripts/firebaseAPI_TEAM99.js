47ab609aed661d15317245fb02b4e56ca6fff119
Arsh Mann <mannarsh3@gmail.com>
2024-02-20 10:03:57 -0800

    (Completed demo07)

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyDX1M_TQMMrp755ka0CdcchlIGn2-EbIBY",
+    authDomain: "comp1800-202410-demo-98d27.firebaseapp.com",
+    projectId: "comp1800-202410-demo-98d27",
+    storageBucket: "comp1800-202410-demo-98d27.appspot.com",
+    messagingSenderId: "154618693706",
+    appId: "1:154618693706:web:406d73057d93e310ee7eb3"
 };
 
 //--------------------------------------------
